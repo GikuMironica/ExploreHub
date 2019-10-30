265e4f70e6b4d0906b25ec9b541aae36afef37b8
Tonislav Tachev <tachevv_@abv.bg>
2019-10-30 17:07:22 +0200

    Added the Home page, added some changes to the login method

---
+++ src/MainUI/mainUiController.java
@@ -0,0 +1,5 @@
+package MainUI;
+
+public class mainUiController {
+
+}
--- src/authentification/AuthentificationController.java
+++ src/authentification/AuthentificationController.java
@@ -29,79 +29,101 @@ public class AuthentificationController {
     @FXML
     private TextField usernameField, passwordField;
     @FXML
-    private Button registerButton;
+    private Button registerButton, loginButton;
 
     private EntityManager em;
 
     public void init(){
         usernameField.setPromptText("Email address");
         passwordField.setPromptText("Password");
+        loginButton.setDisable(true);
+
     }
 
         // process the credentials
     @FXML
-    private void login(){
-            // TEST CONNECTION
+    private void login(Event e) throws IOException{
+        // TEST CONNECTION
         UserConnectionSingleton con = UserConnectionSingleton.getInstance();
         em = con.getManager();
         AdminConnectionSingleton con2 = AdminConnectionSingleton.getInstance();
         em = con2.getManager();
         User u1 = em.find(User.class, 6);
-        System.out.println(u1.getFirstname()+" "+u1.getLastname()+" "+u1.getAccess()+" "+u1.getId()+" "+u1.getPassword());
+        System.out.println(u1.getFirstname() + " " + u1.getLastname() + " " + u1.getAccess() + " " + u1.getId() + " " + u1.getPassword());
 
-            //TEST ENTITY RELATIONS
-        if(u1.getEvents().equals(null))
+        //TEST ENTITY RELATIONS
+        if (u1.getEvents().equals(null))
             System.out.println("null events");
         else {
             List<Events> ev = u1.getEvents();
             System.out.println("Found " + ev.size() + " Events for this user");
         }
-        if(u1.getCourse().equals(null))
+        if (u1.getCourse().equals(null))
             System.out.println("Course Null");
         else
             System.out.println(u1.getCourse().getName());
-        if(u1.getTransactions().equals(null))
+        if (u1.getTransactions().equals(null))
             System.out.println("Transactions Null");
-        else{
+        else {
             List<Transactions> tr = u1.getTransactions();
             System.out.println("Found " + tr.size() + " Transactions for this user");
         }
 
-            // TEST NAMEQUERY FIND USER BY EMAIL
+        // TEST NAMEQUERY FIND USER BY EMAIL
         TypedQuery<User> tq1 = em.createNamedQuery(
                 "User.findUserbyEmail",
-                User.class );
+                User.class);
         tq1.setParameter("email", "Bredesen@hs-ulm.de");
         User u2 = tq1.getSingleResult();
-        System.out.println(u2.getId()+" "+u2.getFirstname()+" "+u2.getLastname());
+        System.out.println(u2.getId() + " " + u2.getFirstname() + " " + u2.getLastname());
 
-            // TEST NAMEDQUERY FIND USER BY GIVEN EMAIL+PASSWORD
+        // TEST NAMEDQUERY FIND USER BY GIVEN EMAIL+PASSWORD
         TypedQuery<User> tq2 = em.createNamedQuery(
                 "User.findUserbyEmailPass",
-                User.class );
+                User.class);
         tq2.setParameter("email", "Giles@hs-ulm.de");
         tq2.setParameter("password", "user8");
         User u3 = tq2.getSingleResult();
-        System.out.println(u3.getId()+" "+u3.getFirstname()+" "+u3.getLastname());
+        System.out.println(u3.getId() + " " + u3.getFirstname() + " " + u3.getLastname());
 
-            // TEST NAMEDQUERY TO FIND ALL ADMINS
+        // TEST NAMEDQUERY TO FIND ALL ADMINS
         TypedQuery<Admin> tq3 = em.createNamedQuery(
                 "Admin.findAdmins",
-                Admin.class );
+                Admin.class);
         tq3.setParameter("access", 1);
         List<Admin> al = tq3.getResultList();
-        for(Admin temp : al){
+        for (Admin temp : al) {
             System.out.println(temp.getEmail());
         }
 
-        /**
-         * if password matches username then proceed to checking the Access Level, initialize an User/Admin account
-         *
-         */
-        //  Fetch the user by PrimaryKey if was found ( ID )
-        //  Admin/Customer c1 = em.find(User.class, id);
+
+        Parent root = FXMLLoader.load(getClass().getResource("../MainUi/mainUi.fxml"));
+        Scene scene = new Scene(root,800,600);
+        scene.getStylesheets().add("/MainUI/style.css");
+        Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
 
 
+    }
+
+
+    @FXML
+    public void handleKeyRelease(){
+        //check if both input fields are not empty, then proceed to login
+        String text = usernameField.getText();
+        String password = passwordField.getText();
+        boolean hasText = text.isEmpty() || text.trim().isEmpty();
+        boolean hasPassword = password.isEmpty() || password.trim().isEmpty();
+        if(hasPassword && hasText) {
+            loginButton.setDisable(hasText);
+        } else if(!hasPassword && !hasText){
+            loginButton.setDisable(false);
+        } else if(hasPassword && !hasText){
+            loginButton.setDisable(true);
+        } else if(!hasPassword && hasText){
+            loginButton.setDisable(true);
+        }
     }
         // Jump to new window, handle register process
     @FXML
