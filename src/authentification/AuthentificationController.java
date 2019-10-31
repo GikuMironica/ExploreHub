a05607da22ae8ed9a50cd5c39ba5abc7374faaaa
Tonislav Tachev <tachevv_@abv.bg>
2019-10-31 13:28:53 +0200

    Implemented the login process, updated the login design!

--- src/authentification/AuthentificationController.java
+++ src/authentification/AuthentificationController.java
@@ -1,5 +1,9 @@
 package authentification;
 
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.concurrent.Service;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,16 +12,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import models.Admin;
 import models.Events;
 import models.Transactions;
 import models.User;
-
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class AuthentificationController {
@@ -26,70 +37,95 @@ public class AuthentificationController {
     private TextField usernameField, passwordField;
     @FXML
     private Button registerButton, loginButton;
+    @FXML
+    private Label alert;
 
     private EntityManager em;
 
-    public void init(){
+    public void init() throws IOException{
         usernameField.setPromptText("Email address");
         passwordField.setPromptText("Password");
         loginButton.setDisable(true);
+        alert.setVisible(false);
+
 
     }
 
-        // process the credentials
     @FXML
     private void login(Event event) throws IOException{
         // TEST CONNECTION
-        UserConnectionSingleton con = UserConnectionSingleton.getInstance();
-        em = con.getManager();
-        AdminConnectionSingleton con2 = AdminConnectionSingleton.getInstance();
-        em = con2.getManager();
-        User u1 = em.find(User.class, 6);
-        System.out.println(u1.getFirstname() + " " + u1.getLastname() + " " + u1.getAccess() + " " + u1.getId() + " " + u1.getPassword());
+//        UserConnectionSingleton con = UserConnectionSingleton.getInstance();
+//        em = con.getManager();
+//        AdminConnectionSingleton con2 = AdminConnectionSingleton.getInstance();
+//        em = con2.getManager();
+//        User u1 = em.find(User.class, 6);
+//        System.out.println(u1.getFirstname() + " " + u1.getLastname() + " " + u1.getAccess() + " " + u1.getId() + " " + u1.getPassword());
+//
+//        //TEST ENTITY RELATIONS
+//        if (u1.getEvents().equals(null))
+//            System.out.println("null events");
+//        else {
+//            List<Events> ev = u1.getEvents();
+//            System.out.println("Found " + ev.size() + " Events for this user");
+//        }
+//        if (u1.getCourse().equals(null))
+//            System.out.println("Course Null");
+//        else
+//            System.out.println(u1.getCourse().getName());
+//        if (u1.getTransactions().equals(null))
+//            System.out.println("Transactions Null");
+//        else {
+//            List<Transactions> tr = u1.getTransactions();
+//            System.out.println("Found " + tr.size() + " Transactions for this user");
+//        }
+//
+//        // TEST NAMEQUERY FIND USER BY EMAIL
+//        TypedQuery<User> tq1 = em.createNamedQuery(
+//                "User.findUserbyEmail",
+//                User.class);
+//        tq1.setParameter("email", "Bredesen@hs-ulm.de");
+//        User u2 = tq1.getSingleResult();
+//        System.out.println(u2.getId() + " " + u2.getFirstname() + " " + u2.getLastname());
 
-        //TEST ENTITY RELATIONS
-        if (u1.getEvents().equals(null))
-            System.out.println("null events");
-        else {
-            List<Events> ev = u1.getEvents();
-            System.out.println("Found " + ev.size() + " Events for this user");
-        }
-        if (u1.getCourse().equals(null))
-            System.out.println("Course Null");
-        else
-            System.out.println(u1.getCourse().getName());
-        if (u1.getTransactions().equals(null))
-            System.out.println("Transactions Null");
-        else {
-            List<Transactions> tr = u1.getTransactions();
-            System.out.println("Found " + tr.size() + " Transactions for this user");
-        }
+        // Try to establish connection as a user
+        try {
+            UserConnectionSingleton con = UserConnectionSingleton.getInstance();
+            em = con.getManager();
 
-        // TEST NAMEQUERY FIND USER BY EMAIL
-        TypedQuery<User> tq1 = em.createNamedQuery(
-                "User.findUserbyEmail",
-                User.class);
-        tq1.setParameter("email", "Bredesen@hs-ulm.de");
-        User u2 = tq1.getSingleResult();
-        System.out.println(u2.getId() + " " + u2.getFirstname() + " " + u2.getLastname());
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+            TypedQuery<User> tq2 = em.createNamedQuery(
+                    "User.findUserbyEmailPass",
+                    User.class);
+            tq2.setParameter("email", username);
+            tq2.setParameter("password", password);
+            User u3 = tq2.getSingleResult();
 
-        // TEST NAMEDQUERY FIND USER BY GIVEN EMAIL+PASSWORD
-        TypedQuery<User> tq2 = em.createNamedQuery(
-                "User.findUserbyEmailPass",
-                User.class);
-        tq2.setParameter("email", "Giles@hs-ulm.de");
-        tq2.setParameter("password", "user8");
-        User u3 = tq2.getSingleResult();
-        System.out.println(u3.getId() + " " + u3.getFirstname() + " " + u3.getLastname());
+            //if user is logged in successfully, open the Home page
 
-        // TEST NAMEDQUERY TO FIND ALL ADMINS
-        TypedQuery<Admin> tq3 = em.createNamedQuery(
-                "Admin.findAdmins",
-                Admin.class);
-        tq3.setParameter("access", 1);
-        List<Admin> al = tq3.getResultList();
-        for (Admin temp : al) {
-            System.out.println(temp.getEmail());
+            Parent root = FXMLLoader.load(getClass().getResource("../MainUi/mainUi.fxml"));
+            Scene scene = new Scene(root,800,600);
+            scene.getStylesheets().add("/MainUI/style.css");
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+
+
+        } catch (Exception exception){
+            System.out.println(exception.getMessage());
+            alert.setText("Unable to login");
+            alert.setVisible(true);
+            usernameField.clear();
+            passwordField.clear();
+            //make the Alert appear and make it disappear after 3 seconds
+            PauseTransition visiblePause = new PauseTransition(
+                    Duration.seconds(3)
+            );
+            visiblePause.setOnFinished(
+                    e -> alert.setVisible(false)
+            );
+            visiblePause.play();
+
         }
 
 
@@ -99,7 +135,15 @@ public class AuthentificationController {
         window.setScene(scene);
         window.show();
 
-
+//        // TEST NAMEDQUERY TO FIND ALL ADMINS
+//        TypedQuery<Admin> tq3 = em.createNamedQuery(
+//                "Admin.findAdmins",
+//                Admin.class);
+//        tq3.setParameter("access", 1);
+//        List<Admin> al = tq3.getResultList();
+//        for (Admin temp : al) {
+//            System.out.println(temp.getEmail());
+//        }
     }
 
 
--- tests/authentification/AuthentificationControllerTest.java
+++ tests/authentification/AuthentificationControllerTest.java
@@ -1,16 +1,54 @@
-package authentification;
+package tests.authentification;
 
-import javafx.fxml.FXML;
+import authentification.UserConnectionSingleton;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import jdk.jfr.Event;
+import models.User;
+import javax.persistence.EntityManager;
 import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+
+import java.awt.*;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 
 public class AuthentificationControllerTest {
 
+   private TextField usernameField;
+   private TextField passwordField;
+   private EntityManager em;
+
     @Test
     public void init() {
-
     }
 
+    @Test
+    public void login(Event e) throws IOException {
+        try {
+            UserConnectionSingleton con = UserConnectionSingleton.getInstance();
+            em = con.getManager();
+
+            String username = usernameField.getText();
+            String password = passwordField.getText();
+            TypedQuery<User> tq2 = em.createNamedQuery(
+                    "User.findUserbyEmailPass",
+                    User.class);
+            tq2.setParameter("email", username);
+            tq2.setParameter("password", password);
+            User u3 = tq2.getSingleResult();
+
+            //if user is logged in successfully, open the Home page
+
+
+        } catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
 }
