4e5367e32293e5c86bb7aca07c0f7b43cbc55d8d
Gheorghe Mironica <mironicagicu@gmail.com>
2019-10-31 14:03:23 +0100

    Update MainUiController.java
    // delete trash

--- src/mainUI/MainUiController.java
+++ src/mainUI/MainUiController.java
@@ -14,26 +14,7 @@ public class MainUiController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        BorderPane root = new BorderPane();
-        FXMLLoader loaderCenter = new FXMLLoader(getClass().getResource("/listComponent/list.fxml"));
-        FXMLLoader loaderTop = new FXMLLoader(getClass().getResource("/barComponent/navbar.fxml"));
-        FXMLLoader loaderLeftTop = new FXMLLoader(getClass().getResource("/filterComponent/filter.fxml"));
-        FXMLLoader loaderLeftBottom = new FXMLLoader(getClass().getResource("/mapComponent/map.fxml"));
-        FXMLLoader loaderRight = new FXMLLoader(getClass().getResource("/sidebarComponent/sidebar.fxml"));
-
-        VBox leftBox = new VBox();
-        leftBox.getChildren().add(loaderLeftTop.getRoot());
-        leftBox.getChildren().add(loaderLeftBottom.getRoot());
-
-        try {
-            root.setCenter(loaderCenter.load());
-            root.setRight(loaderRight.load());
-            root.setLeft(leftBox);
-            root.setTop(loaderTop.load());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+  
     }
 }
 
