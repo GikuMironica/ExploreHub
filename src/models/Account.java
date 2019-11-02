e7ada1095001567b1bb416c34ede2846fa5f41fb
mironicagicu <mironicagicu@gmail.com>
2019-11-02 16:55:47 +0100

    Account Interface updated

--- src/models/Account.java
+++ src/models/Account.java
@@ -4,4 +4,15 @@ import javax.persistence.EntityManager;
 
 public interface Account {
     EntityManager getConnection();
+    int getId();
+    int getAccess();
+    String getEmail();
+    String getFirstname();
+    String getLastname();
+    String getPassword();
+    void setEmail(String email);
+    void setFirstname(String firstname);
+    void setLastname(String lastname);
+    void setPassword(String password);
+
 }
