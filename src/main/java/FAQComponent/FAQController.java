746b75ff8427e7c3c924c8121e08068d0a6af3be
Daniel549 <drakon987@abv.bg>
2019-11-26 23:02:24 +0100

    Added a dependency for webview in the pom file, added FAQ component, updated documentation

--- src/main/java/FAQComponent/FAQController.java
+++ src/main/java/FAQComponent/FAQController.java
@@ -1,14 +1,39 @@
 package FAQComponent;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class FAQController implements Initializable {
+ /**
+ * Frequently asked questions component
+ * @author Daniel Dyankov
+ */
 
+public class FAQController implements Initializable {
+    private WebEngine webengine;
+    private static WebView webview;
+
+    @FXML
+    WebView FAQWebView;
+
+     /**
+      * This method is initializing the webview
+      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        final WebEngine webEngine = FAQWebView.getEngine();
 
+
+        try {
+            webEngine.load("https://explorehub-faq.netlify.com");
+
+
+        } catch (Exception e){
+            System.out.println("FAQ not loading properly");
+        }
     }
 }
+
